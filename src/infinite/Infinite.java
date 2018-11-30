b496f9e488e9089ec22c8621e800b584027580fd
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2018-11-30 12:23:52 -0500

    Add files via upload

---
+++ src/infinite/Infinite.java
@@ -0,0 +1,16 @@
+package infinite;
+
+public class Infinite 
+{
+
+	public static void main(String[] args) 
+	{
+		int integer=1;
+		while(integer==integer)
+			{
+			System.out.println(integer+1);
+			}
+
+	}
+
+}
